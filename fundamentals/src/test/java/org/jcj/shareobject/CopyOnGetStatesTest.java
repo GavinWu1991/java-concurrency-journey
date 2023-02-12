682e63aeb620a2a80c12a25a029db3f5b89b342e
Gavin.Wu <gavin.guohao.wu@kingland.com>
2023-02-12 20:07:31 +0800

    example for copy on get states

--- fundamentals/src/test/java/org/jcj/shareobject/CopyOnGetStatesTest.java
+++ fundamentals/src/test/java/org/jcj/shareobject/CopyOnGetStatesTest.java
@@ -8,16 +8,16 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-class FinalStatesTest extends BaseStatesTest {
+class CopyOnGetStatesTest extends BaseStatesTest{
 
     @Test
-    void shouldDataEscape_WhenAccessStateField() throws InterruptedException {
+    void shouldNoDataEscape_WhenAccessStateField() throws InterruptedException {
         // given an instance of UnsafeStates created and shared to other threads
-        FinalStates finalStates = new FinalStates();
+        CopyOnGetStates copyOnGetStates = new CopyOnGetStates();
 
         List<Thread> threads = IntStream
                 .range(0, NUMBER_OF_THREAD)
-                .mapToObj((idx) -> new Thread(() -> modifyStates(finalStates, idx)))
+                .mapToObj((idx) -> new Thread(() -> modifyStates(copyOnGetStates, idx)))
                 .collect(Collectors.toList());
 
         // When start all thread
@@ -27,11 +27,7 @@ class FinalStatesTest extends BaseStatesTest {
         }
 
         // Then states value of the FinalStates instance should be modified which not equals to the original
-        System.out.println("States in finalStates instance: \n\r"+ Arrays.toString(finalStates.getStates()));
-        Assertions.assertNotSame(EXPECTED_STATES, finalStates.getStates());
-        // And the count of modified thread should not equal to the real number of thread
-        // * because of the race condition
-        Assertions.assertNotEquals(NUMBER_OF_THREAD, finalStates.getStates()[0].indexOf("modified by thread"));
+        System.out.println("States in copyOnGetStates instance: \n\r"+ Arrays.toString(copyOnGetStates.getStates()));
+        Assertions.assertArrayEquals(EXPECTED_STATES, copyOnGetStates.getStates());
     }
-
 }
